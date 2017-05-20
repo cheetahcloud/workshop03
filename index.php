859a19b9c2e4469719fbdea7f7310445142bc920
Etienne Cointet <ecointet@Etiennes-MacBook-Pro.local>
2017-05-20 21:12:03 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<header>
+<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+<style>
+      body {
+        font-family: 'Indie Flower', serif;
+        font-size: 28px;
+      }
+    </style>
+</header>
+<body>
+
+<?php
+
+echo "<div align='center'><img width='400px' src='https://static.wixstatic.com/media/99e1c6_ee5196f474d6417ca79823b2308a2b7f~mv2.png/v1/fill/w_1070,h_274,al_c,usm_0.66_1.00_0.01/99e1c6_ee5196f474d6417ca79823b2308a2b7f~mv2.png' /></div>";
+
+//NEW
+echo phpinfo();
+
+?>
+
+</body>
+</html>
